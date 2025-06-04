6df3a7c527c2c69c3684b4ca8075ce2fecbe4712
xiaoyy20d <lvzhiyuan0925@outlook.com>
2025-06-04 20:15:41 +0800

    Update YAPIS.py

--- SDK/YuanAPI/YAPIS.py
+++ SDK/YuanAPI/YAPIS.py
@@ -6,5 +6,6 @@ class YAPIEngine:
         self.string: Default.string = raw.string
         self.onClicked: Default.onClicked = raw.clicked
         self.LOG: Log = raw
+        self.callBlack: Default.call_block = raw.call_block
         # ---
         self.globals: Globals = raw.API_G
